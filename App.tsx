adcfec0686b8569b5f44b9b4053a003e953ba026
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2020-01-09 13:54:39 +0100

    Separating homescreen from appcontainer

--- App.tsx
+++ App.tsx
@@ -1,26 +1,7 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack'
-import { View, Text, StyleSheet } from 'react-native';
-
-class HomeScreen extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Welcome to Cranberry, the mobile Berrybox app!</Text>
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+import { HomeScreen } from './src/screens/home.screen';
 
 const AppNavigator = createStackNavigator(
     {
