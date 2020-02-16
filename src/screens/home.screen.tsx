4ab872a9016fe3d6fd0079f2c01139ce7c79a9db
AngelZatch <angelzatch@gmail.com>
2020-02-16 15:29:04 +0100

    WiP navigate to box screen

--- src/screens/home.screen.tsx
+++ src/screens/home.screen.tsx
@@ -1,18 +1,55 @@
 import React from "react"
-import { StyleSheet, Text, View } from 'react-native';
-import { BoxListComponent } from "../components/box-list.component";
+import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
 import { Box } from "../models/box.model";
+import BoxCard from "./../components/box-card.component";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export class HomeScreen extends React.Component {
     static navigationOptions = {
         title: 'Home'
     }
 
+    state: {
+        error: any,
+        hasLoadedBoxes: boolean,
+        boxes: Array<Box>
+    } = {
+        error: null,
+        hasLoadedBoxes: false,
+        boxes: []
+    }
+
+    async componentDidMount() {
+        try {
+            const boxes: Array<Box> = await (await fetch('https://araza.berrybox.tv/boxes')).json()
+            this.setState({ boxes, hasLoadedBoxes: true })
+        } catch (error) {
+            this.setState({ error, hasLoadedBoxes: true })
+        }
+    }
+
     render() {
+        console.log('PROPS: ', this.props);
+        const { boxes, hasLoadedBoxes } = this.state
+
         return (
             <View style={styles.container}>
                 <Text style={styles.titlePage}>Boxes</Text>
-                <BoxListComponent></BoxListComponent>
+                {hasLoadedBoxes ? (
+                    <FlatList
+                        data={boxes}
+                        renderItem={({ item, index, separators }) => (
+                            <TouchableOpacity style={styles.card}
+                                onPress={() => console.log('PRESS', item._id)}>
+                                <BoxCard {...item}
+                                ></BoxCard>
+                            </TouchableOpacity>
+                        )}
+                        keyExtractor={(item) => item.name}
+                    />
+                ) : (
+                        <ActivityIndicator />
+                    )}
             </View>
         )
     }
@@ -30,5 +67,8 @@ const styles = StyleSheet.create({
         fontSize: 30,
         marginTop: '1%',
         marginBottom: 10
+    },
+    card: {
+        paddingBottom: 10
     }
 });
