d498813fa14b0e37dd44a7dac866f30d6649713f
AngelZatch <angelzatch@gmail.com>
2020-02-16 12:46:11 +0100

    init BoxScreen

---
+++ src/modules/box/box.screen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+import { Box } from '../../models/box.model';
+
+export class BoxScreen extends React.Component {
+
+
+    render() {
+        return (
+            <View>
+                THIS IS A BOX.
+            </View>
+        )
+    }
+}
