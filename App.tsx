25efab21577b763335e34cbe99f943e4584fa660
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2020-02-17 14:03:51 +0100

    Move box module to screens

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from './src/screens/home.screen';
-import { BoxScreen } from './src/modules/box/box.screen';
+import { BoxScreen } from './src/screens/box/box.screen';
 
 const Stack = createStackNavigator();
 
