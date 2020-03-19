e4c03eb40cae817da2237f369c5e63a50496155c
AngelZatch <angelzatch@gmail.com>
2020-03-19 12:07:52 +0100

    Pull to Refresh boxes

--- src/screens/home.screen.tsx
+++ src/screens/home.screen.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, FlatList, RefreshControl } from 'react-native';
 import { Box } from "../models/box.model";
 import BoxCard from "./../components/box-card.component";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -12,20 +12,29 @@ export class HomeScreen extends React.Component<{navigation}> {
     state: {
         error: any,
         hasLoadedBoxes: boolean,
-        boxes: Array<Box>
+        boxes: Array<Box>,
     } = {
-            error: null,
-            hasLoadedBoxes: false,
-            boxes: []
-        }
+        error: null,
+        hasLoadedBoxes: false,
+        boxes: [],
+    }
 
-    async componentDidMount() {
+    componentDidMount() {
+        this.getBoxes();
+    }
+
+    async getBoxes() {
         try {
             const boxes: Array<Box> = await (await fetch('https://araza.berrybox.tv/boxes')).json()
             this.setState({ boxes, hasLoadedBoxes: true })
         } catch (error) {
             this.setState({ error, hasLoadedBoxes: true })
         }
+    }
+
+    onRefresh() {
+        this.setState({ hasLoadedBoxes: false, boxes: [] })
+        this.getBoxes();
     }
 
     render() {
@@ -37,6 +46,7 @@ export class HomeScreen extends React.Component<{navigation}> {
                 {hasLoadedBoxes ? (
                     <FlatList
                         data={boxes}
+                        refreshControl={<RefreshControl refreshing={!hasLoadedBoxes} onRefresh={this.onRefresh.bind(this)}/>}
                         renderItem={({ item, index, separators }) => (
                             <TouchableOpacity style={styles.card}
                                 onPress={() => this.props.navigation.navigate('Box', {boxToken: item._id})}>
