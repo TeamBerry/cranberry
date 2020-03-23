7e96ad40c056dd6536b305b438d91d0695900d2a
AngelZatch <angelzatch@gmail.com>
2020-03-23 11:38:23 +0100

    align user picture in side menu

--- src/components/custom-menu.component.tsx
+++ src/components/custom-menu.component.tsx
@@ -52,6 +52,7 @@ const styles = StyleSheet.create({
         height: 50,
         width: 50,
         borderRadius: 25,
+        alignSelf: 'center'
     },
     userName: {
         paddingTop: 10,
