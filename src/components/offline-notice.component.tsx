3ca4f0ad46a3a4af4a8e4a50c00d7d9d03bea5b8
AngelZatch <angelzatch@gmail.com>
2020-04-10 19:16:08 +0200

    WiP offline component and socket behavior

---
+++ src/components/offline-notice.component.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react"
+import NetInfo from "@react-native-community/netinfo"
+import { View, Text, StyleSheet } from "react-native"
+
+
+const OfflineNotice = () => {
+
+    const [isConnected, setConnected] = useState(true)
+
+    useEffect(() => {
+        NetInfo.addEventListener(state => setConnected(state.isConnected))
+    })
+
+    if (!isConnected) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>The connexion has been lost. Reconnecting...</Text>
+            </View>
+        )
+    }
+
+    return null
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#B30F4F',
+        height: 30,
+        display: "flex",
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        color: 'white'
+    }
+})
+
+export default OfflineNotice
