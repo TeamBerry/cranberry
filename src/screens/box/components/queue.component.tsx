e8bd1703f1587e77040dcd59a2065d4879f1fec5
AngelZatch <angelzatch@gmail.com>
2020-04-11 17:34:05 +0200

    Display of force next

--- src/screens/box/components/queue.component.tsx
+++ src/screens/box/components/queue.component.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react"
 import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image, Animated } from "react-native"
-import { SyncPacket } from "@teamberry/muscadine"
+import { SyncPacket, QueueItem } from "@teamberry/muscadine"
 import { Box } from "../../../models/box.model"
 import Collapsible from 'react-native-collapsible'
 import ProfilePicture from './../../../components/profile-picture.component'
@@ -83,17 +83,28 @@ const Queue = ({ box, currentVideo }: Props) => {
             )
         }
 
+        // Put the preslected video first
+        const preselectedVideoIndex = upcomingVideos.findIndex((item: QueueItem) => item.isPreselected)
+        if (preselectedVideoIndex !== -1) {
+            const preselectedVideo = upcomingVideos[preselectedVideoIndex]
+            upcomingVideos.splice(preselectedVideoIndex, 1)
+            upcomingVideos.unshift(preselectedVideo)
+        }
+
         return (
             <FlatList
                 data={upcomingVideos}
                 renderItem={({ item }) => (
                     <View style={styles.upcomingItem}>
                         <Image
-                            style={{ width: 88.89, height: 60 }}
+                            style={[item.isPreselected ? styles.preselectedVideo : {}, { width: 88.89, height: 60 }]}
                             source={{uri: `https://i.ytimg.com/vi/${item.video.link}/hqdefault.jpg`}}
                         />
                         <View style={{ paddingLeft: 10, width: 240 }}>
-                            <Text style={styles.upcomingItemName} numberOfLines={2}>{item.video.name}</Text>
+                            <Text style={styles.upcomingItemName} numberOfLines={2}>
+                                <Text style={styles.nextItemName}>{item.isPreselected ? 'NEXT: ' : null}</Text>
+                                {item.video.name}
+                            </Text>
                             <View style={{ paddingLeft: 5, flex: 1, flexDirection: 'row' }}>
                                 <ProfilePicture userId={item.submitted_by._id} size={20}/>
                                 <Text style={{paddingLeft: 5, color: '#BBBBBB'}}>{item.submitted_by.name}</Text>
@@ -161,6 +172,10 @@ const styles = StyleSheet.create({
     currentSpaceActions: {
         width: 40
     },
+    preselectedVideo: {
+        borderColor: '#EBBA17',
+        borderWidth: 2
+    },
     boxName: {
         color: '#BBBBBB',
         fontFamily: 'Montserrat-Regular'
@@ -184,6 +199,10 @@ const styles = StyleSheet.create({
     upcomingItemName: {
         fontFamily: 'Montserrat-Regular',
         color: 'white',
+    },
+    nextItemName: {
+        fontFamily: 'Montserrat-SemiBold',
+        color: '#EBBA17'
     }
 })
 
