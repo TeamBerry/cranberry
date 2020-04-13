2d16477c6c60cb9706cf72a83799c250e5626685
AngelZatch <angelzatch@gmail.com>
2020-04-13 23:38:59 +0200

    WiP Buttons

--- src/screens/box/components/queue-list.component.tsx
+++ src/screens/box/components/queue-list.component.tsx
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View, FlatList } from "react-native"
 import { QueueItem } from "@teamberry/muscadine"
 import { Box } from "../../../models/box.model"
 import QueueVideo from './queue-video.component'
+import { SwipeListView, SwipeRow } from 'react-native-swipe-list-view'
+import { RectButton } from 'react-native-gesture-handler'
+import { Svg, Polygon, Rect } from 'react-native-svg';
 
 export type Props = {
     box: Box
@@ -13,7 +16,7 @@ const QueueList = ({ box }: Props) => {
 
     useEffect(() => {
         const buildUpcomingVideos = () => {
-            console.log('BUILDING THIS SHIT')
+            console.log('BUILDING QUEUE', new Date())
             if (!box) {
                 return
             }
@@ -46,6 +49,57 @@ const QueueList = ({ box }: Props) => {
         buildUpcomingVideos()
     }, box?.playlist)
 
+    const BoxButtons = () => {
+        return (
+            <>
+                <View style={{width: 160, backgroundColor: 'red'}}>
+            <RectButton style={[styles.action, styles.dangerAction]}>
+                <Text style={{color: 'white'}}>X</Text>
+            </RectButton>
+            <RectButton style={[styles.action, styles.rightAction]} >
+            <Svg height="30" width="30" style={{scaleX: 0.7, scaleY: 0.7}}>
+                <Polygon
+                    points="24,20 9,10 9,30"
+                    fill="white"
+                    />
+                <Polygon
+                    points="39,20 24,10 24,30"
+                    fill="white"
+                />
+                    </Svg>
+                    </RectButton>
+                </View>
+            </>
+        )
+    }
+
+    const UserButtons = () => {
+        return (
+            <RectButton style={[styles.action, styles.rightAction]}>
+                <Svg height="33" width="33" style={{scaleX: 0.8, scaleY: 0.8}}>
+                    <Rect x="4" y="8" width="32" height="2" fill="white" />
+                    <Rect x="4" y="13.5" width="32" height="2" fill="white" />
+                    <Rect x="4" y="19" width="16" height="2" fill="white" />
+                    <Rect x="4" y="24.5" width="16" height="2" fill="white" />
+                    <Rect x="4" y="30" width="16" height="2" fill="white" />
+                    <Rect x="22" y="24.5" width="14" height="2" fill="white" />
+                    <Rect x="28" y="19" width="2" height="14" fill="white" />
+                </Svg>
+            </RectButton>
+        )
+    }
+
+    const actionButtons = () => {
+        return (
+            <>
+                <View style={styles.rowBack}>
+                    <BoxButtons />
+                    <UserButtons />
+                </View>
+            </>
+        )
+    }
+
     if (!box) {
         return (<></>)
     }
@@ -55,12 +109,17 @@ const QueueList = ({ box }: Props) => {
     }
 
     return (
-        <FlatList
+        <SwipeListView
+            useFlatList={true}
             data={upcomingVideos}
             renderItem={({ item }) => (
-                <QueueVideo item={item}/>
+                <View style={styles.rowFront}>
+                    <QueueVideo item={item} />
+                </View>
             )}
-            initialNumToRender={6}
+            renderHiddenItem={actionButtons}
+            leftOpenValue={80}
+            rightOpenValue={-80}
             ItemSeparatorComponent={() => <View style={styles.separator} />}
             keyExtractor={(item, index) => index.toString()}
         />
@@ -71,7 +130,42 @@ const styles = StyleSheet.create({
     separator: {
         height: StyleSheet.hairlineWidth,
         backgroundColor: '#191919'
+    },
+    action: {
+        width: 35,
+        height: 35,
+        borderRadius: 17.5,
+        display: "flex",
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        marginHorizontal: 5
+    },
+    dangerAction: {
+        backgroundColor: '#B30F4F',
+    },
+    rightAction: {
+        backgroundColor: '#AAA',
+    },
+    actionIcon: {
+        width: 30,
+        marginHorizontal: 10
+    },
+    rowFront: {
+        alignItems: 'center',
+        backgroundColor: '#262626',
+        justifyContent: 'center',
+    },
+    rowBack: {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     }
 })
 
-export default QueueList
+const areEqual = (prevProps, nextProps) => {
+    return JSON.stringify(prevProps.box?.playlist) === JSON.stringify(nextProps.box?.playlist)
+}
+
+export default React.memo(QueueList, areEqual)
--- src/screens/box/components/queue-video.component.tsx
+++ src/screens/box/components/queue-video.component.tsx
@@ -2,9 +2,7 @@ import React, { useRef, useEffect } from "react"
 import { StyleSheet, Text, View, Image } from "react-native"
 import { QueueItem } from "@teamberry/muscadine"
 import ProfilePicture from '../../../components/profile-picture.component'
-import Swipeable from 'react-native-gesture-handler/Swipeable'
-import { RectButton } from 'react-native-gesture-handler'
-import { Svg, Polygon, Rect } from 'react-native-svg';
+import { SwipeRow } from 'react-native-swipe-list-view'
 
 export type Props = {
     item: QueueItem
@@ -12,106 +10,28 @@ export type Props = {
 
 
 const QueueVideo = ({ item }: Props) => {
-    const _swipeRef = useRef(null)
-
-    useEffect(() => {
-        console.log(`COMPONENT ${item._id} HAS RENDERED.`)
-    })
-
-    const renderLeftActions = (progress, dragX) => {
-        // const scale = dragX.interpolate({
-        //     inputRange: [0, 80],
-        //     outputRange: [0, 1],
-        //     extrapolate: 'clamp'
-        // })
-
-        return (
-            <>
-            <RectButton
-                style={[styles.action, styles.leftAction]}
-                onPress={closeSwipe}
-            >
-                <Text style={{color: 'white'}}>X</Text>
-            </RectButton>
-            <RectButton
-                style={[styles.action, styles.rightAction]}
-                onPress={closeSwipe}
-            >
-                <Svg height="30" width="30" style={{scaleX: 0.7, scaleY: 0.7}}>
-                    <Polygon
-                        points="24,20 9,10 9,30"
-                        fill="white"
-                        />
-                    <Polygon
-                        points="39,20 24,10 24,30"
-                        fill="white"
-                    />
-                    </Svg>
-            </RectButton>
-            </>
-        )
-    }
-
-    const renderRightActions = (progress, dragX) => {
-        // const scale = dragX.interpolate({
-        //     inputRange: [-80, 0],
-        //     outputRange: [1, 0],
-        //     extrapolate: 'clamp'
-        // })
-
-        return (
-            <RectButton
-                style={[styles.action, styles.rightAction]}
-                onPress={closeSwipe}
-            >
-                <Svg height="33" width="33" style={{scaleX: 0.8, scaleY: 0.8}}>
-                    <Rect x="4" y="8" width="32" height="2" fill="white" />
-                    <Rect x="4" y="13.5" width="32" height="2" fill="white" />
-                    <Rect x="4" y="19" width="16" height="2" fill="white" />
-                    <Rect x="4" y="24.5" width="16" height="2" fill="white" />
-                    <Rect x="4" y="30" width="16" height="2" fill="white" />
-                    <Rect x="22" y="24.5" width="14" height="2" fill="white" />
-                    <Rect x="28" y="19" width="2" height="14" fill="white" />
-                </Svg>
-            </RectButton>
-        )
-    }
-
-    const closeSwipe = () => {
-        _swipeRef.current.close()
-    }
-
     return (
-        <Swipeable
-            ref={_swipeRef}
-            friction={3}
-            leftThreshold={40}
-            rightThreshold={40}
-            renderLeftActions={renderLeftActions}
-            renderRightActions={renderRightActions}
-        >
-            <View style={styles.queueVideo}>
-                <Image
-                    style={[
-                        item.isPreselected ? styles.preselectedVideo : {},
-                        item.startTime ? styles.currentVideo : {},
-                        { width: 88.89, height: 60 }
-                    ]}
-                    source={{uri: `https://i.ytimg.com/vi/${item.video.link}/default.jpg`}}
-                    />
-                <View style={{ paddingLeft: 10, width: 240 }}>
-                    <Text style={styles.queueVideoName} numberOfLines={2}>
-                        <Text style={styles.nextVideoIndicator}>{item.isPreselected ? 'NEXT: ' : null}</Text>
-                        <Text style={styles.currentVideoIndicator}>{item.startTime !== null ? 'PLAYING: ' : null}</Text>
-                        {item.video.name}
-                    </Text>
-                    <View style={{ paddingLeft: 5, flex: 1, flexDirection: 'row' }}>
-                        <ProfilePicture userId={item.submitted_by._id} size={20}/>
-                        <Text style={{paddingLeft: 5, color: '#BBBBBB'}}>{item.submitted_by.name}</Text>
-                    </View>
+        <View style={styles.queueVideo}>
+            <Image
+                style={[
+                    item.isPreselected ? styles.preselectedVideo : {},
+                    item.startTime ? styles.currentVideo : {},
+                    { width: 88.89, height: 60 }
+                ]}
+                source={{uri: `https://i.ytimg.com/vi/${item.video.link}/default.jpg`}}
+                />
+            <View style={{ paddingLeft: 10, width: 240 }}>
+                <Text style={styles.queueVideoName} numberOfLines={2}>
+                    <Text style={styles.nextVideoIndicator}>{item.isPreselected ? 'NEXT: ' : null}</Text>
+                    <Text style={styles.currentVideoIndicator}>{item.startTime !== null ? 'PLAYING: ' : null}</Text>
+                    {item.video.name}
+                </Text>
+                <View style={{ paddingLeft: 5, flex: 1, flexDirection: 'row' }}>
+                    <ProfilePicture userId={item.submitted_by._id} size={20}/>
+                    <Text style={{paddingLeft: 5, color: '#BBBBBB'}}>{item.submitted_by.name}</Text>
                 </View>
             </View>
-        </Swipeable>
+        </View>
     )
 }
 
@@ -141,31 +61,7 @@ const styles = StyleSheet.create({
     nextVideoIndicator: {
         fontFamily: 'Montserrat-SemiBold',
         color: '#EBBA17'
-    },
-    action: {
-        width: 35,
-        height: 35,
-        borderRadius: 17.5,
-        display: "flex",
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center',
-        marginHorizontal: 5
-    },
-    leftAction: {
-        backgroundColor: '#B30F4F',
-    },
-    rightAction: {
-        backgroundColor: '#AAA',
-    },
-    actionIcon: {
-        width: 30,
-        marginHorizontal: 10
-    },
+    }
 })
 
-const areEqual = (prevProps: Props, nextProps: Props) => {
-    return prevProps.item.isPreselected === nextProps.item.isPreselected
-}
-
-export default React.memo(QueueVideo, areEqual)
+export default QueueVideo
