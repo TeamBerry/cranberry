d59fae443411ae0eabba909cc1eb644641848789
AngelZatch <angelzatch@gmail.com>
2020-05-25 12:29:27 +0200

    chore: fix duration pipe indent

--- src/shared/duration.pipe.ts
+++ src/shared/duration.pipe.ts
@@ -1,16 +1,20 @@
 const durationToString = (value: string) => {
-    let duration = '';
+  let duration = '';
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [_, years, days, hours, mins, secs] = value.match(/PT(?:(\d+)Y)?(?:(\d+)D)?(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/);
-
-    if (years) { duration += `${years}y `; }
-    if (days) { duration += `${days}d `; }
-    if (hours) { duration += `${hours}h `; }
-    if (mins) { duration += `${mins}min `; }
-    if (secs) { duration += `${secs}s`; }
-
+  if (!value) {
     return duration;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, years, days, hours, mins, secs] = value.match(/PT(?:(\d+)Y)?(?:(\d+)D)?(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/);
+
+  if (years) { duration += `${years}y `; }
+  if (days) { duration += `${days}d `; }
+  if (hours) { duration += `${hours}h `; }
+  if (mins) { duration += `${mins}min `; }
+  if (secs) { duration += `${secs}s`; }
+
+  return duration;
 };
 
 export default durationToString;
