5a8f0d82f19f9b1a00a5fdd29cb6382db627cbc8
AngelZatch <angelzatch@gmail.com>
2020-09-15 17:37:07 +0200

    feat: countdown & indicator on search

---
+++ src/components/countdown-indicator.component.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  defaultStyle: {
+    height: 2,
+  },
+});
+
+const CountdownIndicator = (props: { time: number, color?: string, text: string }) => {
+  const { time, color, text } = props;
+
+  const [width, setWidth] = useState(100);
+  let displayTimeout;
+  const widthFraction = (1 / (time / 1000)) * 100;
+
+  useEffect(() => {
+    displayTimeout = setInterval(() => {
+      setWidth((width) => width - widthFraction);
+    }, 1000);
+
+    return () => clearInterval(displayTimeout);
+  }, []);
+
+  return (
+    <>
+      <Text style={{ textAlign: 'center', color: '#BBBBBB', paddingVertical: 5 }}>{text}</Text>
+      { width > 0 ? (
+        <View style={[styles.defaultStyle, { width: `${width}%`, backgroundColor: color ?? '#009AEB' }]} />
+      ) : null}
+    </>
+  );
+};
+
+CountdownIndicator.defaultProps = {
+  color: '#009AEB',
+};
+
+export default CountdownIndicator;
