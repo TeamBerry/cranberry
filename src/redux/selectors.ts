d6bea0d04ce85772e9d50b5c31e0c33da5396a7e
AngelZatch <angelzatch@gmail.com>
2020-12-06 23:39:21 +0100

    refacto: rename user reducer in merge

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -1,3 +1,3 @@
-export const getSettings = (store) => store.userReducer.settings;
+export const getSettings = (store) => store.user.settings;
 
-export const getUser = (store) => store.userReducer;
+export const getUser = (store) => store.user;
