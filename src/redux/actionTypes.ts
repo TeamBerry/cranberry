fe4487e064e00ca173e5f173dfb9c07c552f139c
AngelZatch <angelzatch@gmail.com>
2020-12-07 15:22:09 +0100

    refacto: merge reducers

--- src/redux/actionTypes.ts
+++ src/redux/actionTypes.ts
@@ -1,4 +1,4 @@
 export const UPDATE_USER = 'UPDATE_USER';
-export const REFRESH_TOKEN = 'REFRESH_TOKEN';
+export const RESTORE_TOKEN = 'RESTORE_TOKEN';
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
