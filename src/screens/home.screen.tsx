cca9288a605bf219bcbf36f4da87de7907ef8f75
AngelZatch <angelzatch@gmail.com>
2021-03-02 13:36:03 +0100

    1.0.0

--- src/screens/home.screen.tsx
+++ src/screens/home.screen.tsx
@@ -33,7 +33,7 @@ const HomeScreen = (props: { navigation, user: AuthSubject, picture: string }) =
 
   const checkCompatibility = async () => {
     try {
-      await axios.post(`${Config.API_URL}/auth/compat`, { version: '0.17.0' });
+      await axios.post(`${Config.API_URL}/auth/compat`, { version: '1.0.0' });
     } catch (error) {
       setAllowed(false);
     }
